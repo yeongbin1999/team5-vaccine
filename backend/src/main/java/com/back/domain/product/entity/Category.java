2e7fef7a8723f484a29936f8f1c3c4e3e2ae2b46
loseminho <loseminho@gmail.com>
2025-07-15 15:46:40 +0900

    product 패키지 틀, product, category 엔티티 생성

---
+++ backend/src/main/java/com/back/domain/product/entity/Category.java
@@ -0,0 +1,30 @@
+package com.back.domain.product.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Category {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "parent_id")
+    private Category parent;
+
+    @OneToMany(mappedBy = "parent", cascade = CascadeType.ALL)
+    private List<Category> categories = new ArrayList<>();
+}
---
+++ backend/src/main/java/com/back/domain/product/repository/ProductRepository.java
@@ -0,0 +1,4 @@
+package com.back.domain.product.repository;
+
+public class ProductRepository {
+}
