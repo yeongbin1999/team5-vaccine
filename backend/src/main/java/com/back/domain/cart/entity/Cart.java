8a9cd61d911b0d8b861efc82269faa0d7e519d0d
darancode <darancode@gmail.com>
2025-07-16 15:28:58 +0900

    fix(be): CartItem IcCa 티티와 dto 전체적인 리팩토링

--- backend/src/main/java/com/back/domain/cart/entity/Cart.java
+++ backend/src/main/java/com/back/domain/cart/entity/Cart.java
@@ -25,4 +25,8 @@ public class Cart {
     @OneToMany(mappedBy = "cart", cascade = CascadeType.ALL, orphanRemoval = true)
     @Builder.Default
     private List<CartItem> cartItems = new ArrayList<>();
+
+    public void clearItems() {
+        cartItems.clear();
+    }
 }
