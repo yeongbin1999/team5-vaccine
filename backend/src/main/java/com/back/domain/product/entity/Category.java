0d268f639eb853e6da3a91c8263a1c902e51037f
loseminho <loseminho@gmail.com>
2025-07-16 15:00:36 +0900

    카테고리 엔티티 수정

--- backend/src/main/java/com/back/domain/product/entity/Category.java
+++ backend/src/main/java/com/back/domain/product/entity/Category.java
@@ -6,6 +6,7 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Entity
@@ -22,7 +23,24 @@ public class Category {
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "parent_id")
+    private Category parent;
 
-    @OneToMany(mappedBy = "category", cascade = CascadeType.REMOVE)
-    private List<Category> categories;
+
+    // 하위 카테고리 목록
+    @OneToMany(mappedBy = "parent", cascade = {CascadeType.PERSIST, CascadeType.REMOVE})
+    private  List<Category> children = new ArrayList<>(); // 초기화 (nullpointexception 방지)
+
+    // 현재 카테고리에 속한 상품 목록
+    @OneToMany(mappedBy = "category", cascade = {CascadeType.PERSIST, CascadeType.REMOVE})
+    private  List<Product> products = new ArrayList<>(); // 초기화 (nullpointexception 방지)
+
+    // 양방향 관계 설정 시 사용하기 위한 편의 메서드
+    public void addChildCategory(Category category){
+        this.children.add(category);
+        category.setParent(this);
+    }
+    public void setParent(Category parent) {
+        this.parent = parent;
+    }
+
 }
