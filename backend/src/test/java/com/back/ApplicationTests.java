73903cf92ee450284103958110e967540fdd6d7d
yeongbin1999 <yeongbin1999@gmail.com>
2025-07-16 14:20:13 +0900

    fix(be): 엔티티 불일치 수정

--- backend/src/test/java/com/back/ApplicationTests.java
+++ backend/src/test/java/com/back/ApplicationTests.java
@@ -2,10 +2,8 @@ package com.back;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootTest
-@EnableJpaAuditing
 class ApplicationTests {
 
     @Test
