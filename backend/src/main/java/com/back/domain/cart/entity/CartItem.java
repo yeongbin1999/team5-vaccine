19113e863ce9f31e11075e6d8e141423eceffb53
daran2 <129312040+daran2@users.noreply.github.com>
2025-07-16 15:41:43 +0900

    fix(be): CartItem Id Long 데이터타입 Integer로 변경 (#20)

--- backend/src/main/java/com/back/domain/cart/entity/CartItem.java
+++ backend/src/main/java/com/back/domain/cart/entity/CartItem.java
@@ -12,7 +12,7 @@ import lombok.*;
 public class CartItem {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+    private Integer id;
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "cart_id", nullable = false)
--- backend/src/main/java/com/back/domain/cart/repository/CartRepository.java
+++ backend/src/main/java/com/back/domain/cart/repository/CartRepository.java
@@ -6,6 +6,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
-public interface CartRepository extends JpaRepository<Cart, Long> {
+public interface CartRepository extends JpaRepository<Cart, Integer> {
     Optional<Cart> findByUser(User user);
 }
