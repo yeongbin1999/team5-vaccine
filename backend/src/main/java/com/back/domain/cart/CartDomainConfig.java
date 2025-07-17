9986bda12ddf6cfbb3e6df3ff7d93b1cddc60264
daran2 <129312040+daran2@users.noreply.github.com>
2025-07-17 14:44:24 +0900

    feat(be):  CartDomainConfig 컨픽파일 추가 (#34)
    * fix(be): CartItem IcCa 티티와 dto 전체적인 리팩토링
    
    * feat(be) implementing CartService
    
    * feat(be) adding DTO files naming AddCartItemRequest, UpdateCartItemRequest and refactoring CartService
    
    * feat(be) implementing CartController
    
    * feat(be):Adding CartDomainConfig

---
+++ backend/src/main/java/com/back/domain/cart/CartDomainConfig.java
@@ -0,0 +1,9 @@
+package com.back.domain.cart;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(basePackages = "com.back.domain.cart")
+public class CartDomainConfig {
+}
