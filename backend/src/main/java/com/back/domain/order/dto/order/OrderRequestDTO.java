f531074cac1d6afa25d314cd4757ee2146a61f02
Code <yna4371@gmail.com>
2025-07-19 23:29:11 +0900

    fix(be): OrderService 수정

--- backend/src/main/java/com/back/domain/order/dto/order/OrderRequestDTO.java
+++ backend/src/main/java/com/back/domain/order/dto/order/OrderRequestDTO.java
@@ -1,16 +1,13 @@
 package com.back.domain.order.dto.order;
 
 import com.back.domain.order.dto.orderitem.OrderItemRequestDTO;
-import lombok.Getter;
-import lombok.Setter;
 
 import java.util.List;
 
-@Getter
-@Setter
-public class OrderRequestDTO {
-    private int userId;
-    private int deliveryId;
-    private String shippingAddress;
-    private List<OrderItemRequestDTO> items;
-}
+public record OrderRequestDTO(
+        int userId,
+        int deliveryId,
+        String shippingAddress,
+        List<OrderItemRequestDTO> items
+) {}
+
