50b96f428477d4f57b970bb80a7afc5eb236e85a
daran2 <129312040+daran2@users.noreply.github.com>
2025-07-19 22:11:14 +0900

    feat(be): Admin 공통 API 구현을 위한 Dto, Config, Service,Controller 구현 / feat(be): UserRepository에 findByEmail 기능 추가 / feat(be) OrderRepository에 findByOrderDateBetween, findAllOrderItemsWithProduct 기능 추가 (#55)
    * feat(be): 관ë¦Admi API 구현을 위한 Dto, Config, Service 구현
    
    * 관리자 통합 API 기능 구현을 위한 AdminController 구현

---
+++ backend/src/main/java/com/back/domain/admin/AdminDomainConfig.java
@@ -0,0 +1,10 @@
+package com.back.domain.admin;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(basePackages = "com.back.domain.admin")
+public class AdminDomainConfig {
+
+}
